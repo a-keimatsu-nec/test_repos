daa8906eaaea49b4f440a632e831a20dddc027f6
a-keimatsu-nec <a-keimatsu@cp.jp.nec.com>
2017-02-02 17:02:10 +0900

    GitHubへのpush設定

---
+++ scripts/release_push.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# gitユーザ設定
+git config --global user.email "a-keimatsu@cp.jp.nec.com" #コミットユーザの有効なメールアドレス
+git config --global user.name "Akitsugu Keimatsu" #コミットユーザの名前
+
+# リリースバージョン名
+CI_RELEASE_VERSION = ` date + "v%Y%m%d%H%M%S" `
+
+# リリースブランチ名
+CI_RELEASE_BRANCH = "release/${CI_RELEASE_VERSION}"
+
+# リモートリポジトリ
+CI_REMOTE_REPOSITORY = "git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git"
+
+# リリースブランチ作成
+git checkout -b ${ CI_RELEASE_BRANCH }
+
+#変更ファイルををすべて追加
+git add --all :/build/
+
+#コミット
+git commit -m "[auto] release branch (${CI_RELEASE_VERSION})"
+
+#リモートにプッシュ
+git push ${ CI_REMOTE_REPOSITORY } ${ CI_RELEASE_BRANCH }
+
